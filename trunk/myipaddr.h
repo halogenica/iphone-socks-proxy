966425abff27e5120c2f9f082b734dbd351341bf
udi.benreuven <udi.benreuven@27038a46-eb3e-11de-a198-37193048d1ed>
2009-12-17 19:38:41 +0000

    copyright

--- trunk/myipaddr.h
+++ trunk/myipaddr.h
@@ -1,10 +1,4 @@
 /*
- *  myipaddr.h
- *  SimpleNetworkStreams
- *
- *  Created by Ehud Ben-Reuven on 12/17/09.
- *  Copyright 2009 __MyCompanyName__. All rights reserved.
- *
  */
 
 char * myipaddr(void);
