5a11a6002ff230413a52d7614bcb4e95e8945c52
udi.benreuven <udi.benreuven@27038a46-eb3e-11de-a198-37193048d1ed>
2009-12-17 19:03:37 +0000

    1st

---
+++ SocksProxyController.m
@@ -0,0 +1,354 @@
+/*
+*/
+
+#import "SocksProxyController.h"
+
+#import "AppDelegate.h"
+
+#include <CFNetwork/CFNetwork.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "myipaddr.h"
+
+@interface SocksProxyController ()
+
+// Properties that don't need to be seen by the outside world.
+
+@property (nonatomic, readonly) BOOL                isStarted;
+@property (nonatomic, retain)   NSNetService *      netService;
+@property (nonatomic, assign)   CFSocketRef         listeningSocket;
+@property (nonatomic, assign)   NSInteger			nConnections;
+@property (nonatomic, readonly) SocksProxy **       sendreceiveStream;
+
+// Forward declarations
+
+- (void)_stopServer:(NSString *)reason;
+
+@end
+
+@implementation SocksProxyController
+@synthesize nConnections  = _nConnections;
+// Because sendreceiveStream is declared as an array, you have to use a custom getter.  
+// A synthesised getter doesn't compile.
+
+- (SocksProxy **)sendreceiveStream
+{
+    return self->_sendreceiveStream;
+}
+
+#pragma mark * Status management
+
+// These methods are used by the core transfer code to update the UI.
+
+- (void)_serverDidStartOnPort:(int)port
+{
+    assert( (port > 0) && (port < 65536) );
+    //self.statusLabel.text = [NSString stringWithFormat:@"%d.%d.%d.%d %d", (ipaddr>>24),0xff&(ipaddr>>16),0xff&(ipaddr>>8),0xff&ipaddr, port];
+    self.statusLabel.text = @"Started";
+    self.portLabel.text = [NSString stringWithFormat:@"%d", port];
+	self.addressLabel.text =[NSString stringWithCString:myipaddr() encoding:[NSString defaultCStringEncoding]];
+
+    [self.startOrStopButton setTitle:@"Stop" forState:UIControlStateNormal];
+    self.tabBarItem.image = [UIImage imageNamed:@"receiveserverOn.png"];
+}
+
+- (void)_serverDidStopWithReason:(NSString *)reason
+{
+    if (reason == nil) {
+        reason = @"Stopped";
+    }
+	self.addressLabel.text=@"";
+	self.portLabel.text=@"";
+    self.statusLabel.text = reason;
+    [self.startOrStopButton setTitle:@"Start" forState:UIControlStateNormal];
+    self.tabBarItem.image = [UIImage imageNamed:@"receiveserverOff.png"];
+}
+
+- (void)_sendreceiveDidStart
+{
+    self.statusLabel.text = @"Receiving";
+    [self.activityIndicator startAnimating];
+    [[AppDelegate sharedAppDelegate] didStartNetworking];
+}
+
+- (void)_updateStatus:(NSString *)statusString
+{
+    assert(statusString != nil);
+    self.statusLabel.text = statusString;
+}
+
+- (void)_sendreceiveDidStopWithStatus:(NSString *)statusString
+{
+    if (statusString == nil) {
+        statusString = @"Receive succeeded";
+    }
+    self.statusLabel.text = statusString;
+    [self.activityIndicator stopAnimating];
+    [[AppDelegate sharedAppDelegate] didStopNetworking];
+}
+
+#pragma mark * Core transfer code
+
+// This is the code that actually does the networking.
+
+@synthesize netService      = _netService;
+@synthesize listeningSocket = _listeningSocket;
+
+- (BOOL)isStarted
+{
+    return (self.netService != nil);
+}
+
+// Have to write our own setter for listeningSocket because CF gets grumpy 
+// if you message NULL.
+
+- (void)setListeningSocket:(CFSocketRef)newValue
+{
+    if (newValue != self->_listeningSocket) {
+        if (self->_listeningSocket != NULL) {
+            CFRelease(self->_listeningSocket);
+        }
+        self->_listeningSocket = newValue;
+        if (self->_listeningSocket != NULL) {
+            CFRetain(self->_listeningSocket);
+        }
+    }
+}
+
+- (void)_acceptConnection:(int)fd
+{
+	int i;
+	for(i=0;i<self.nConnections;i++)
+		if ( ! self.sendreceiveStream[i].isSendingReceiving )
+			break;
+	
+	if(i>=self.nConnections) {
+		if(i>NCONNECTIONS) {
+			close(fd);
+			return;
+		}
+		self.sendreceiveStream[i] = [[SocksProxy alloc] init];
+		self.sendreceiveStream[i].delegate = self;
+		self.nConnections++;
+	}
+	
+	[self.sendreceiveStream[i] startSendReceive:fd];
+}
+
+static void AcceptCallback(CFSocketRef s, CFSocketCallBackType type, CFDataRef address, const void *data, void *info)
+    // Called by CFSocket when someone connects to our listening socket.  
+    // This implementation just bounces the request up to Objective-C.
+{
+    SocksProxyController *  obj;
+    
+    #pragma unused(type)
+    assert(type == kCFSocketAcceptCallBack);
+    #pragma unused(address)
+    // assert(address == NULL);
+    assert(data != NULL);
+    
+    obj = (SocksProxyController *) info;
+    assert(obj != nil);
+
+    #pragma unused(s)
+    assert(s == obj->_listeningSocket);
+    
+    [obj _acceptConnection:*(int *)data];
+}
+
+- (void)netService:(NSNetService *)sender didNotPublish:(NSDictionary *)errorDict
+    // A NSNetService delegate callback that's called if our Bonjour registration 
+    // fails.  We respond by shutting down the server.
+    //
+    // This is another of the big simplifying assumptions in this sample. 
+    // A real server would use the real name of the device for registrations, 
+    // and handle automatically renaming the service on conflicts.  A real 
+    // client would allow the user to browse for services.  To simplify things 
+    // we just hard-wire the service name in the client and, in the server, fail 
+    // if there's a service name conflict.
+{
+    #pragma unused(sender)
+    assert(sender == self.netService);
+    #pragma unused(errorDict)
+    
+    [self _stopServer:@"Registration failed"];
+}
+
+- (void)_startServer
+{
+    BOOL        success;
+    int         err;
+    int         fd;
+    int         junk;
+    struct sockaddr_in addr;
+    int         port;
+	
+	self.nConnections=0;
+    // Create a listening socket and use CFSocket to integrate it into our 
+    // runloop.  We bind to port 0, which causes the kernel to give us 
+    // any free port, then use getsockname to find out what port number we 
+    // actually got.
+
+    port = 50000;
+    
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    success = (fd != -1);
+    
+    if (success) {
+        memset(&addr, 0, sizeof(addr));
+        addr.sin_len    = sizeof(addr);
+        addr.sin_family = AF_INET;
+        addr.sin_port   = htons(port);
+        addr.sin_addr.s_addr = INADDR_ANY;
+        err = bind(fd, (const struct sockaddr *) &addr, sizeof(addr));
+        success = (err == 0);
+    }
+    if (success) {
+        err = listen(fd, 5);
+        success = (err == 0);
+    }
+    if (success) {
+        socklen_t   addrLen;
+
+        addrLen = sizeof(addr);
+        err = getsockname(fd, (struct sockaddr *) &addr, &addrLen);
+        success = (err == 0);
+        
+        if (success) {
+            assert(addrLen == sizeof(addr));
+            port = ntohs(addr.sin_port);
+        }
+    }
+    if (success) {
+        CFSocketContext context = { 0, self, NULL, NULL, NULL };
+        
+        self.listeningSocket = CFSocketCreateWithNative(
+            NULL, 
+            fd, 
+            kCFSocketAcceptCallBack, 
+            AcceptCallback, 
+            &context
+        );
+        success = (self.listeningSocket != NULL);
+        
+        if (success) {
+            CFRunLoopSourceRef  rls;
+            
+            CFRelease(self.listeningSocket);        // to balance the create
+
+            fd = -1;        // listeningSocket is now responsible for closing fd
+
+            rls = CFSocketCreateRunLoopSource(NULL, self.listeningSocket, 0);
+            assert(rls != NULL);
+            
+            CFRunLoopAddSource(CFRunLoopGetCurrent(), rls, kCFRunLoopDefaultMode);
+            
+            CFRelease(rls);
+        }
+    }
+
+    // Now register our service with Bonjour.  See the comments in -netService:didNotPublish: 
+    // for more info about this simplifying assumption.
+
+    if (success) {
+        //self.netService = [[[NSNetService alloc] initWithDomain:@"local." type:@"_x-SNSUpload._tcp." name:@"Test" port:port] autorelease];
+        self.netService = [[[NSNetService alloc] initWithDomain:@"" type:@"_socks5._tcp." name:@"Test" port:port] autorelease];
+        success = (self.netService != nil);
+    }
+    if (success) {
+        self.netService.delegate = self;
+        
+        [self.netService publishWithOptions:NSNetServiceNoAutoRename];
+        
+        // continues in -netServiceDidPublish: or -netService:didNotPublish: ...
+    }
+    
+    // Clean up after failure.
+    
+    if ( success ) {
+        assert(port != 0);
+        [self _serverDidStartOnPort:port];
+    } else {
+        [self _stopServer:@"Start failed"];
+        if (fd != -1) {
+            junk = close(fd);
+            assert(junk == 0);
+        }
+    }
+}
+
+- (void)_stopServer:(NSString *)reason
+{
+	int i;
+	for (i=0; i<self.nConnections; i++) {
+		if (self.sendreceiveStream[i].isSendingReceiving)
+			[self.sendreceiveStream[i] stopSendReceiveWithStatus:@"Cancelled"];
+    }
+	
+    if (self.netService != nil) {
+        [self.netService stop];
+        self.netService = nil;
+    }
+    if (self.listeningSocket != NULL) {
+        CFSocketInvalidate(self.listeningSocket);
+        self.listeningSocket = NULL;
+    }
+    [self _serverDidStopWithReason:reason];
+}
+
+
+#pragma mark * Actions
+
+- (IBAction)startOrStopAction:(id)sender
+{
+    #pragma unused(sender)
+    if (self.isStarted) {
+        [self _stopServer:nil];
+    } else {
+        [self _startServer];
+    }
+}
+
+#pragma mark * View controller boilerplate
+
+@synthesize addressLabel       = _addressLabel;
+@synthesize portLabel       = _portLabel;
+@synthesize statusLabel       = _statusLabel;
+@synthesize activityIndicator = _activityIndicator;
+@synthesize startOrStopButton = _startOrStopButton;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    assert(self.statusLabel != nil);
+    assert(self.activityIndicator != nil);
+    assert(self.startOrStopButton != nil);
+    
+    self.activityIndicator.hidden = YES;
+    self.statusLabel.text = @"Tap Start to start the server";
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    self.statusLabel = nil;
+    self.activityIndicator = nil;
+    self.startOrStopButton = nil;
+}
+
+- (void)dealloc
+{
+    [self _stopServer:nil];
+	int i;
+	for(i=0;i<self.nConnections;i++)
+		[self.sendreceiveStream[i] dealloc];
+    
+    [self->_statusLabel release];
+    [self->_activityIndicator release];
+    [self->_startOrStopButton release];
+
+    [super dealloc];
+}
+
+@end
---
+++ myipaddr.c
@@ -0,0 +1,84 @@
+/*
+ *  myipaddr.c
+ *  SimpleNetworkStreams
+ *
+ *  Created by Ehud Ben-Reuven on 12/17/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "myipaddr.h"
+
+/*
+ *  ipaddr.h
+ *  SimpleNetworkStreams
+ *
+ *  Created by Ehud Ben-Reuven on 12/17/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ifaddrs.h>
+#include <string.h>
+#include <stdbool.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <net/if_dl.h>
+#include <arpa/inet.h>
+#include <ifaddrs.h>
+
+#if ! defined(IFT_ETHER)
+#define IFT_ETHER 0x6/* Ethernet CSMACD */
+#endif
+
+char * myipaddr(void)
+{
+	bool success;
+	struct ifaddrs *addrs;
+	const struct ifaddrs *cursor;
+	const struct sockaddr_dl *dlAddr;
+	const uint8_t *base;
+	int i;
+	const struct ifaddrs * en0cursor=NULL;
+	
+	success = getifaddrs(&addrs) == 0;
+	if (success) {
+		cursor = addrs;
+		while (cursor != NULL) {
+			if ((cursor->ifa_flags & IFF_LOOPBACK) == 0 ) {
+				printf("%s ", (char *)cursor->ifa_name);
+				printf("%s\n",inet_ntoa(((struct sockaddr_in *)cursor->ifa_addr)->sin_addr));
+				if(!strcmp((char*)cursor->ifa_name,"en0"))
+					en0cursor=cursor;
+			}
+			if ( (cursor->ifa_addr->sa_family == AF_LINK)
+				&& (((const struct sockaddr_dl *) cursor->ifa_addr)->sdl_type ==IFT_ETHER)
+				) {
+				dlAddr = (const struct sockaddr_dl *) cursor->ifa_addr;
+				//      fprintf(stderr, " sdl_nlen = %d\n", dlAddr->sdl_nlen);
+				//      fprintf(stderr, " sdl_alen = %d\n", dlAddr->sdl_alen);
+				base = (const uint8_t *) &dlAddr->sdl_data[dlAddr->sdl_nlen];
+				printf(" MAC address ");
+				for (i = 0; i < dlAddr->sdl_alen; i++) {
+					if (i != 0) {
+						printf(":");
+					}
+					printf("%02x", base[i]);
+				} 
+				printf("\n");
+			}
+			cursor = cursor->ifa_next;
+		}
+	}
+	if(en0cursor!=NULL) {
+		char *s = inet_ntoa(((struct sockaddr_in *)en0cursor->ifa_addr)->sin_addr);
+		if (strcmp(s,"6.3.6.0"))
+			return s;
+	}
+	return "no WiFi";
+}
